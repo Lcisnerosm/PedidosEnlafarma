8cbf59fac16917da0f0199ad5e1c68d1789ee1f6
Lcisnerosm <53229664+Lcisnerosm@users.noreply.github.com>
2020-11-18 19:59:39 -0600

    cambios

--- src/app/Component/pedidos/pedidos.component.ts
+++ src/app/Component/pedidos/pedidos.component.ts
@@ -4,19 +4,16 @@ import { PedidosService } from '../../services/pedidos.service';
 @Component({
   selector: 'app-pedidos',
   templateUrl: './pedidos.component.html',
-  styleUrls: ['./pedidos.component.css']
+  styleUrls: ['./pedidos.component.css'],
 })
 export class PedidosComponent implements OnInit {
-  listadepedidos: any = []
-  data
-  constructor(private pedidosservice: PedidosService) { }
+  listadepedidos: any = [];
+  data;
+  constructor(private pedidosservice: PedidosService) {}
 
   async ngOnInit() {
     console.log(await this.pedidosservice.getProducts());
     this.listadepedidos = await this.pedidosservice.getProducts();
     this.data = this.listadepedidos.data;
   }
-
-
-
 }
--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -2,22 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PedidosService {
-
-  constructor(private http: HttpClient, ) { }
+  constructor(private http: HttpClient) {}
 
   getProducts() {
-
-    return new Promise(async resolve=> {
+    return new Promise(async (resolve) => {
       const headers = new HttpHeaders({
-        'authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1MWE0OTAwOC1hZjA0LTRlZmItOTY2Zi1hY2JjMjZhZDU0MWUiLCJyb2wiOiJhZG1pbiIsImlkdmVuZG9yIjoxLCJpYXQiOjE2MDU3NDg0OTIsImV4cCI6MTYwNTc4MDg5Mn0.oG7r-YBcNrh8ckTPcEYi4vvLisBeW8ZbC-dbSRE2aRI`
-      })
-      this.http.get(`https://backendenlafarma.herokuapp.com/api/recep`, {headers})
-      .subscribe( response => {
-        resolve(response);
-      })
-    })
+        authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1MWE0OTAwOC1hZjA0LTRlZmItOTY2Zi1hY2JjMjZhZDU0MWUiLCJyb2wiOiJhZG1pbiIsImlkdmVuZG9yIjoxLCJpYXQiOjE2MDU3NDg0OTIsImV4cCI6MTYwNTc4MDg5Mn0.oG7r-YBcNrh8ckTPcEYi4vvLisBeW8ZbC-dbSRE2aRI`,
+      });
+      this.http
+        .get(`https://backendenlafarma.herokuapp.com/api/recep`, { headers })
+        .subscribe((response) => {
+          resolve(response);
+        });
+    });
   }
 }
